5521b3b488ba5964a62d6d56cd6509bee4a72e2a
Orodiel <orodiel@gmail.com>
2017-08-01 16:27:19 +0300

    added check if annotation target is a method

--- src/core/lombok/javac/handlers/HandleToggleProvider.java
+++ src/core/lombok/javac/handlers/HandleToggleProvider.java
@@ -1,7 +1,10 @@
 package lombok.javac.handlers;
 
+import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.JCTree.JCAnnotation;
+import com.sun.tools.javac.tree.JCTree.JCMethodDecl;
 import lombok.ToggleProvider;
+import lombok.core.AST;
 import lombok.core.AnnotationValues;
 import lombok.javac.JavacAnnotationHandler;
 import lombok.javac.JavacNode;
@@ -11,6 +14,14 @@ import org.mangosdk.spi.ProviderFor;
 public class HandleToggleProvider extends JavacAnnotationHandler<ToggleProvider> {
     @Override
     public void handle(AnnotationValues<ToggleProvider> annotation, JCAnnotation ast, JavacNode annotationNode) {
-        //TODO: implement
+        JavacNode owner = annotationNode.up();
+        if (owner.getKind() != AST.Kind.METHOD) {
+            annotationNode.addError("@ToggleProvider is legal only on methods.");
+        }
+
+        //TODO: add return type check (should only return boolean)
+        /*((JCMethodDecl) owner.get()).getReturnType();*/
+
+        //TODO: add arguments check (should only accept String)
     }
 }
