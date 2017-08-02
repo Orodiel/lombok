1351add6801fada9c2b7724b3fa27726d4b15dd0
Orodiel <orodiel@gmail.com>
2017-08-02 14:20:10 +0300

    WOW IT SOMEHOW WORKS!1111!

--- src/core/lombok/javac/handlers/HandleToggle.java
+++ src/core/lombok/javac/handlers/HandleToggle.java
@@ -2,35 +2,151 @@ package lombok.javac.handlers;
 
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.JCTree.JCAnnotation;
-import com.sun.tools.javac.tree.JCTree.JCMethodDecl;
-import com.sun.tools.javac.tree.JCTree.JCStatement;
-import com.sun.tools.javac.tree.JCTree.JCExpression;
-import com.sun.tools.javac.tree.JCTree.JCMethodInvocation;
-import com.sun.tools.javac.tree.JCTree.JCBlock;
 import com.sun.tools.javac.tree.JCTree.JCClassDecl;
-import com.sun.tools.javac.tree.JCTree.JCVariableDecl;
+import com.sun.tools.javac.tree.JCTree.JCMethodDecl;
+import com.sun.tools.javac.tree.JCTree.JCMethodInvocation;
+import com.sun.tools.javac.tree.JCTree.JCExpression;
+import com.sun.tools.javac.tree.JCTree.JCStatement;
+import com.sun.tools.javac.tree.JCTree.JCBlock;
 
 import com.sun.tools.javac.util.Name;
 import lombok.Toggle;
-import lombok.core.AST;
 import lombok.core.AnnotationValues;
 import lombok.javac.Javac;
 import lombok.javac.JavacAnnotationHandler;
 import lombok.javac.JavacNode;
 import lombok.javac.JavacTreeMaker;
-import netscape.javascript.JSException;
 import org.mangosdk.spi.ProviderFor;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import static com.sun.tools.javac.util.List.nil;
 import static lombok.Toggle.ToggleState.*;
 import static lombok.javac.handlers.JavacHandlerUtil.*;
 
 @ProviderFor(JavacAnnotationHandler.class)
 public class HandleToggle extends JavacAnnotationHandler<Toggle> {
+
+    static class TogglePair {
+        public JCMethodDecl on;
+        public JCMethodDecl off;
+        public JCMethodDecl oldOn;
+        public JCMethodDecl oldOff;
+    }
+
+    @Override
+    public void handle(AnnotationValues<Toggle> annotation, JCAnnotation ast, JavacNode annotationNode) {
+        JavacNode classDeclWrapper = annotationNode.up().up();
+        JCClassDecl classDecl = (JCClassDecl) classDeclWrapper.get();
+//        Map<String, TogglePair> toggles = findAllToggles(classDecl);
+        Map<String, TogglePair> toggles = new HashMap<String, TogglePair>();
+        deleteAnnotationIfNeccessary(annotationNode, Toggle.class);
+        TogglePair pair = new TogglePair();
+        pair.on = (JCMethodDecl) annotationNode.up().get();
+        toggles.put("Test", pair);
+        for (TogglePair togglePair : toggles.values()) {
+            handleToggle(togglePair, classDeclWrapper);
+        }
+    }
+
+    private void handleToggle(TogglePair togglePair, JavacNode classDecl) {
+        validateTogglePairDefinition(togglePair);
+        renameOldMethods(togglePair, classDecl);
+        constructNewMethods(togglePair, classDecl);
+        injectNewMethods(togglePair);
+    }
+
+    private void injectNewMethods(TogglePair togglePair) {
+
+    }
+
+    private void constructNewMethods(TogglePair togglePair, JavacNode classDeclWrappper) {
+        JCClassDecl classDecl = (JCClassDecl) classDeclWrappper.get();
+//        com.sun.tools.javac.util.List<JCTree> members = classDecl.getMembers();
+//        JCMethodDecl stateGetter = null;
+//        for (JCTree member : members) {
+//            if (member instanceof JCMethodDecl) {
+//                JCMethodDecl methodDecl = (JCMethodDecl) member;
+//                if (methodDecl.getName().toString().endsWith("getToggleState")) {
+//                    stateGetter = methodDecl;
+//                    break;
+//                }
+//            }
+//        }
+//        if (stateGetter == null) {
+//            classDeclWrappper.addError("No method \"getToggleState\" was found.");
+//        }
+
+        // body generation:
+        JavacTreeMaker maker = classDeclWrappper.getTreeMaker();
+        Name aThis = classDeclWrappper.toName("this");
+        Name getter = classDeclWrappper.toName("getToggleState");
+
+        JCMethodInvocation toggleStateEvaluationInvocation = maker.Apply(
+                com.sun.tools.javac.util.List.<JCExpression>nil(),
+                maker.Select(maker.Ident(aThis), getter),
+                com.sun.tools.javac.util.List.of((JCExpression) maker.Literal("Test"))
+        );
+
+        JCMethodInvocation invokeThis = maker.Apply(
+                com.sun.tools.javac.util.List.<JCExpression>nil(),
+                maker.Select(maker.Ident(aThis), classDeclWrappper.toName("test1")),
+                com.sun.tools.javac.util.List.<JCExpression>nil()
+        );
+
+        JCMethodInvocation invokeThat = maker.Apply(
+                com.sun.tools.javac.util.List.<JCExpression>nil(),
+                maker.Select(maker.Ident(aThis), classDeclWrappper.toName("test2")),
+                com.sun.tools.javac.util.List.<JCExpression>nil()
+        );
+
+        Toggle.ToggleState state = ON;
+        com.sun.tools.javac.util.List<JCStatement> statements = com.sun.tools.javac.util.List.of(
+                (JCStatement)
+                maker.If(
+                        maker.Binary(
+                                Javac.CTC_EQUAL,
+                                toggleStateEvaluationInvocation,
+                                maker.Literal(Javac.CTC_BOOLEAN, 1)
+                        ),
+                        maker.Exec((state ==  ON) ? invokeThis : invokeThat),
+                        maker.Exec((state == OFF) ? invokeThis : invokeThat)
+                )
+        );
+
+        JCBlock newBody = maker.Block(0, statements);
+        //
+
+        JCMethodDecl newMethod = maker.MethodDef(
+                togglePair.on.mods,
+                classDeclWrappper.toName(togglePair.on.name.toString() + "_new"),
+                togglePair.on.restype,
+                togglePair.on.typarams,
+                togglePair.on.params,
+                togglePair.on.thrown,
+                newBody,
+                togglePair.on.defaultValue
+        );
+
+
+        injectMethod(classDeclWrappper, newMethod);
+    }
+
+    private void renameOldMethods(TogglePair togglePair, JavacNode classDecl) {
+        System.out.println("Pair.on.name: [" + togglePair.on.name + "]");
+        System.out.println("JavacNode.toName: [" + classDecl.toName(togglePair.on.name.toString() + "_test") + "]");
+        System.out.println("Ident: " + classDecl.getTreeMaker().Ident(togglePair.on.name));
+        System.out.println("New Ident: " + classDecl.getTreeMaker().Ident(classDecl.toName(togglePair.on.name.toString() + "_test")));
+    }
+
+    private void validateTogglePairDefinition(TogglePair togglePair) {
+        System.out.println("Validation: skipped for now");
+    }
+
+    private Map<String, TogglePair> findAllToggles(JCClassDecl classDecl) {
+        return new HashMap<String, TogglePair>();
+    }
+
 
     // 1) check if target is a method [done]
     // 1.5) delete annotation
@@ -38,8 +154,7 @@ public class HandleToggle extends JavacAnnotationHandler<Toggle> {
     // 3) do signature compatibility check (skip for now)
     // 4) rename methods to "method"_$old or something and access modifiers
     // 5) replace old methods with evaluation plus if statement; (only for Toggle for now)
-    @Override
-    public void handle(AnnotationValues<Toggle> annotation, JCAnnotation ast, JavacNode annotationNode) {
+    /*public void handle(AnnotationValues<Toggle> annotation, JCAnnotation ast, JavacNode annotationNode) {
         // 1) check if target is a method
         JavacNode owner = annotationNode.up();
         if (owner.getKind() != AST.Kind.METHOD) {
@@ -47,17 +162,26 @@ public class HandleToggle extends JavacAnnotationHandler<Toggle> {
         }
 
         // 1.5) delete annotation
-        deleteAnnotationIfNeccessary(annotationNode, Toggle.class);
+//        deleteAnnotationIfNeccessary(annotationNode, Toggle.class);
 
         // 2) find mirror annotation/method
+        System.out.println("AST: [" + ast + "]");
+        System.out.println("args: [" + ast.args + "]");
+        System.out.println("annotation type: [" + ast.annotationType + "]");
+        System.out.println("class decl?: [" + annotationNode.up().up() + "]");
+
+//        Map<String, TogglePair> toggles = findAllToggles(annotationNode.up().up()); // supposed to be class decl
+
         Toggle toggle = annotation.getInstance();
         String toggleName = toggle.value();
         Toggle.ToggleState state = toggle.state();
 
         List<Toggle> toggles = new ArrayList<Toggle>();
         Collection<JavacNode> fields = annotationNode.upFromAnnotationToFields();
+        System.out.println(fields.size());
         JavacNode targetField = null;
         for (JavacNode field : fields) {
+            System.out.println("" + field + ": " + field.getName());
             JCMethodDecl method = (JCMethodDecl) field.get();
             com.sun.tools.javac.util.List<JCAnnotation> annotations = method.getModifiers().annotations;
             for (JCAnnotation methodAnnotation : annotations) {
@@ -73,10 +197,12 @@ public class HandleToggle extends JavacAnnotationHandler<Toggle> {
 
         if (toggles.size() != 1) {
             annotationNode.addError("Must be exactly two @Toggle with same toggle name.");
+            return;
         }
         Toggle oppositeToggle = toggles.get(0);
         if (oppositeToggle.state() == state) {
             annotationNode.addError("Opposite @Toggle must have opposite state.");
+            return;
         }
 
         // toggle
@@ -164,5 +290,6 @@ public class HandleToggle extends JavacAnnotationHandler<Toggle> {
 
 
         injectMethod(owner.up(), newMethod);
-    }
+    }*/
+
 }
